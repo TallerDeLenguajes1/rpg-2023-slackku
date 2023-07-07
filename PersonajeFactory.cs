00eee9868af7ce1a9abe57bea1dd9f22554b4ba7
slacku <bananuya3@gmail.com>
2023-07-07 20:28:21 -0300

    Fixes on min stats and custom ages raceBased

--- PersonajeFactory.cs
+++ PersonajeFactory.cs
@@ -11,20 +11,37 @@ namespace PersonajeSpace
             personaje.Nombre = Constantes.nombres[rnd.Next(0, Constantes.nombres.Length - 1)];
             personaje.Apodo = Constantes.apodos[rnd.Next(0, Constantes.apodos.Length - 1)];
             personaje.Raza = Constantes.razas[rnd.Next(0, Constantes.razas.Length - 1)];
-            
-            // Edad random
-            personaje.Edad = rnd.Next(15, 73);
+
+            // Edad random en base a raza
+            switch (personaje.Raza)
+            {
+                case "Humano":
+                    personaje.Edad = rnd.Next(16, 80);
+                    break;
+                case "Elfo":
+                    personaje.Edad = rnd.Next(16, 300);
+                    break;
+                case "Goblin":
+                    personaje.Edad = rnd.Next(16, 100);
+                    break;
+                case "DemiHumano":
+                    personaje.Edad = rnd.Next(16, 150);
+                    break;
+                case "Enano":
+                    personaje.Edad = rnd.Next(16, 200);
+                    break;
+            }
             // Obtencion de año de nacimiento
             DateTime now = DateTime.Now;
             int yearNac = now.Year - personaje.Edad;
             int[] dayMonth = { rnd.Next(1, 12), rnd.Next(1, 31) };
             personaje.FecNac = new DateTime(yearNac, dayMonth[0], dayMonth[1]);
-
-            personaje.Velocidad = rnd.Next(0, 10);
-            personaje.Destreza = rnd.Next(0, 5);
-            personaje.Fuerza = rnd.Next(0, 10);
-            personaje.Nivel = rnd.Next(0, 10);
-            personaje.Armadura = rnd.Next(0, 10);
+            // Stats random
+            personaje.Velocidad = rnd.Next(1, 10);
+            personaje.Destreza = rnd.Next(1, 5);
+            personaje.Fuerza = rnd.Next(1, 10);
+            personaje.Nivel = rnd.Next(1, 10);
+            personaje.Armadura = rnd.Next(1, 10);
             personaje.Salud = 100;
 
             return personaje;
