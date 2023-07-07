f95800e07520ddd7d1f5aa38a8bbe8612676b525
slacku <bananuya3@gmail.com>
2023-07-07 20:26:24 -0300

    Add test names

--- Constantes.cs
+++ Constantes.cs
@@ -2,8 +2,40 @@ namespace PersonajeSpace
 {
     public static class Constantes
     {
-        public static string[] nombres = { "Samira", "Akshan", "Viktor", "Syndra" };
-        public static string[] apodos = { "The Master Piece", "The Mad Scientist", "The Unkillable Demon King", "The Random One" };
-        public static string[] razas = { "Goblin", "Elfo", "Humano", "DemiHumano" };
+        public static string[] nombres = {
+            "Samir",
+            "Akshan",
+            "Vik",
+            "Synd",
+            "Irel",
+            "Relu",
+            "Banshe",
+            "Niche",
+            "Xatti",
+            "Velichuf Jr.",
+            "Pugna",
+            "Senko",
+            "Kriffy",
+            "Lexiam",
+            "Khasante"
+        };
+        public static string[] apodos = {
+            "The Master Piece",
+            "The Mad Scientist",
+            "The Unkillable Demon King",
+            "The Random One",
+            "The Rogue Sentinel",
+            "The Empress of the Void",
+            "The Crazy One",
+            "The True Hell",
+            "The Desert Rose",
+            "The Crazy Paws",
+            "The ButterHands",
+            "The Slippery Crab",
+            "The Dark Knight",
+            "The Eminence in Shadow",
+            "The Storm Crow"
+        };
+        public static string[] razas = { "Goblin", "Elfo", "Humano", "Vastaya", "Yordle" };
     }
 }
