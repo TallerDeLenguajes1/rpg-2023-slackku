7fe3b80953dffb6a83438452b65a37fb1ffef79c
slacku <bananuya3@gmail.com>
2023-07-13 15:13:46 -0300

    Races changed, with his custom ages

--- PersonajeFactory.cs
+++ PersonajeFactory.cs
@@ -7,35 +7,54 @@ namespace PersonajeSpace
         {
             Random rnd = new Random();
             Personaje personaje = new Personaje();
+            string region = Constantes.regiones[rnd.Next(0, Constantes.regiones.Length - 1)];
+            int indexRandomPersonaje = rnd.Next(0, 7);
 
-            personaje.Nombre = Constantes.nombres[rnd.Next(0, Constantes.nombres.Length - 1)];
-            personaje.Apodo = Constantes.apodos[rnd.Next(0, Constantes.apodos.Length - 1)];
-            personaje.Raza = Constantes.razas[rnd.Next(0, Constantes.razas.Length - 1)];
+            personaje.Nombre = Constantes.personajeData[region][indexRandomPersonaje][0];
+            personaje.Apodo = Constantes.personajeData[region][indexRandomPersonaje][1];
+            personaje.Raza = Constantes.personajeData[region][indexRandomPersonaje][2];
+            personaje.Region = region;
 
             // Edad random en base a raza
+            int edad = 0;
             switch (personaje.Raza)
             {
-                case "Humano":
-                    personaje.Edad = rnd.Next(16, 80);
+                case "Human":
+                case "Human & Yeti":
+                    edad = rnd.Next(20, 80);
+                    if (personaje.Raza.Equals("Human & Yeti")) edad = 9; // Nunu is a kid with a yeti
                     break;
-                case "Elfo":
-                    personaje.Edad = rnd.Next(16, 300);
+                case "Darkin":
+                    edad = rnd.Next(3500, 6000);
                     break;
-                case "Goblin":
-                    personaje.Edad = rnd.Next(16, 100);
+                case "Golem":
+                    if (personaje.Nombre.Equals("Orianna")) edad = rnd.Next(21, 60);
+                    if (personaje.Nombre.Equals("Galio")) edad = rnd.Next(648, 705);
                     break;
-                case "DemiHumano":
-                    personaje.Edad = rnd.Next(16, 150);
+                case "Spirit God":
+                    edad = 10000;
                     break;
-                case "Enano":
-                    personaje.Edad = rnd.Next(16, 200);
+                case "Revenant":
+                    edad = rnd.Next(16, 500);
+                    break;
+                case "Undead":
+                    edad = rnd.Next(122, 179);
+                    break;
+                case "Yordle":
+                    edad = 0;
+                    break;
+                case "Vastaya":
+                    edad = rnd.Next(180, 250);
+                    break;
+                case "Ascendant":
+                    edad = rnd.Next(900, 3000);
                     break;
             }
-            // Obtencion de año de nacimiento
-            DateTime now = DateTime.Now;
-            int yearNac = now.Year - personaje.Edad;
-            int[] dayMonth = { rnd.Next(1, 12), rnd.Next(1, 31) };
-            personaje.FecNac = new DateTime(yearNac, dayMonth[0], dayMonth[1]);
+            personaje.Edad = edad;
+            // Presente aproximado de noxus: 997 AN (2023 irl)
+            // Obtencion de año de nacimiento - En Años Runaterra: BN | AN (Before/After Noxus)
+
+            personaje.FecNac = new int[] { rnd.Next(1, 12), rnd.Next(1, 31), (int)Single.Abs(Constantes.presente - edad) };
             // Stats random
             personaje.Velocidad = rnd.Next(1, 10);
             personaje.Destreza = rnd.Next(1, 5);
